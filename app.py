da32f7adc1b1227ac18ddfed8c9be0ef237b97b2
klong46 <kyledlong1@gmail.com>
2026-01-21 11:23:20 -0800

    refactor keyword endpoint

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
-import math
+from flask import Flask, request
 from markupsafe import escape
 
-import wiki_service
+LINK_HARD_LIMIT = 5
+
+import link_service
 
 app = Flask(__name__)
 
@@ -10,7 +11,11 @@ app = Flask(__name__)
 def home():
     return "<p>Welcome to the Link Tree application!</p>"
 
-@app.route("/wiki/<target>")
-def wiki(target):
+@app.route("/find_keyword/<target>")
+def find_keyword(target):
+    click_limit = LINK_HARD_LIMIT
+    url_click_limit = request.args.get('click_limit', default=LINK_HARD_LIMIT, type=int)
+    if url_click_limit <= LINK_HARD_LIMIT:
+        click_limit = url_click_limit
     target_string = f"{escape(target)}"
-    return wiki_service.get_shortest_path(target_string)
+    return link_service.get_shortest_path(target_string, click_limit)
